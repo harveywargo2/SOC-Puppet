805c2440c4c47c22571822e0e5cc68bcd5fd04b6
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-01-23 16:44:36 -0500

    net kql logic refactor

--- socpuppet/detectpy/kql_qbuild.py
+++ socpuppet/detectpy/kql_qbuild.py
@@ -1,14 +1,17 @@
 
 
-def kql_single_table_query_builder(data, time_period):
-    logic = data.get('kqlTable') + f' | where Timestamp >= ago({time_period})' + data.get('kqlQuery')
+def kql_single_table_builder(data, time_period):
+    kql = data.get('kqlDetect')
+    kql_var = kql.get('kqlVar')
+    kql_table = kql.get('kqlTable')
+    kql_query = kql.get('kqlQuery')
+
+    if kql_var is not None:
+        logic = kql_var + kql_table + f' | where Timestamp >= ago({time_period})' + kql_query
+    else:
+        logic = kql_table + f' | where Timestamp >= ago({time_period})' + kql_query
+
     output = {'query': logic}
+
     return output
-
-
-def kql_single_table_var_query_builder(data, time_period):
-    logic = data.get('kqlVar') + data.get('kqlTable') + f' | where Timestamp >= ago({time_period})' + data.get('kqlQuery')
-    output = {'query': logic}
-    return output
-
 
---
+++ socpuppet/detectpy/windows_os/net/net_references.md
@@ -0,0 +1,16 @@
+# Net Command Line Utility in Windows
+
+Net Command Line Utility is commonly used by Cyber Threat Actors to 
+1. Perform AD Enumeration
+2. Discovery of Users Information
+3. Discovery of System Information 
+4. Discovery of Password Policy
+4. Modify Local Groups & Users
+
+
+## References
+- [Mitre Attack Software - S0039](https://attack.mitre.org/software/S0039/)
+- https://learn.microsoft.com/en-US/troubleshoot/windows-server/networking/net-commands-on-operating-systems
+- https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-xp/bb490717(v=technet.10)?redirectedfrom=MSDN
+- https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-xp/bb490718(v=technet.10)
+
