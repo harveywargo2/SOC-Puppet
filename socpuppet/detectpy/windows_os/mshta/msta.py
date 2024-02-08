469ea692d4fa6fe3b5b41bf4a3c5229273c1fe00
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-02-08 07:52:15 -0500

    update

---
+++ socpuppet/detectpy/windows_os/mshta/msta.py
@@ -0,0 +1,24 @@
+import os
+import yaml
+import socpuppet.detectpy.kql_qbuild as kbuild
+
+
+# Path Variables for Module
+mshta_path = os.path.dirname(os.path.abspath(__file__))
+mshta_kql_path = os.path.join(mshta_path, 'kql')
+mshta_sigma_path = os.path.join(mshta_path, 'sigma')
+
+
+def mshta(*, type='kql', kql_ago='1d'):
+
+    if type == 'kql':
+        with open(os.path.join(mshta_kql_path,
+                               'netsh_p0001_disable_windows_firewall.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        output = kbuild.kql_single_table_builder(data, kql_ago)
+    else:
+        output = f'type={type} not supported'
+
+    return output
+
