d73c9dc6da38c316564a91a1c54c8d35081c8a32
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-02-10 14:57:05 -0500

    uppdate

--- socpuppet/detectpy/__init__.py
+++ socpuppet/detectpy/__init__.py
@@ -3,9 +3,11 @@
 from socpuppet.detectpy.windows_os.cmstp.cmstp import *
 from socpuppet.detectpy.windows_os.control.control import *
 from socpuppet.detectpy.windows_os.hh.hh import *
+from socpuppet.detectpy.windows_os.infdefaultinstall.infdefaultinstall import *
 from socpuppet.detectpy.windows_os.mavinject.mavinject import *
 from socpuppet.detectpy.windows_os.msdt.msdt import *
+from socpuppet.detectpy.windows_os.mshta.mshta import *
 from socpuppet.detectpy.windows_os.net.net import *
 from socpuppet.detectpy.windows_os.netsh.netsh import *
 from socpuppet.detectpy.windows_os.nltest.nltest import *
-
+from socpuppet.detectpy.windows_os.wuauclt.wuauclt import *
