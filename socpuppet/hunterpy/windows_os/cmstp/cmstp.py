694c001fada09b0233af09fcc1651960dcbc9f42
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-02-24 12:31:43 -0500

    cmstp update

--- socpuppet/hunterpy/windows_os/cmstp/cmstp.py
+++ socpuppet/hunterpy/windows_os/cmstp/cmstp.py
@@ -9,11 +9,39 @@ cmstp_kql_path = os.path.join(cmstp_path, 'm365d')
 cmstp_sigma_path = os.path.join(cmstp_path, 'sigma')
 
 
-def cmstp_p1000_executing_inf_file(*, type='m365d', kql_ago='1d'):
+def cmstp_p1000_executing_inf_file_silent(*, type='m365d', kql_ago='1d'):
 
     if type == 'm365d':
         with open(os.path.join(cmstp_kql_path,
-                               'cmstp_p1000_executing_inf_file.yaml'), 'r') as file:
+                               'cmstp_p1000_executing_inf_file_silent.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        output = kbuild.kql_single_table_builder(data, kql_ago, time_field='Timestamp')
+    else:
+        output = f'type={type} not supported'
+
+    return output
+
+
+def cmstp_p1001_executing_inf_file(*, type='m365d', kql_ago='1d'):
+
+    if type == 'm365d':
+        with open(os.path.join(cmstp_kql_path,
+                               'cmstp_p1001_executing_inf_file.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        output = kbuild.kql_single_table_builder(data, kql_ago, time_field='Timestamp')
+    else:
+        output = f'type={type} not supported'
+
+    return output
+
+
+def cmstp_p1002_executing_inf_file_all_profiles(*, type='m365d', kql_ago='1d'):
+
+    if type == 'm365d':
+        with open(os.path.join(cmstp_kql_path,
+                               'cmstp_p1002_executing_inf_all_profiles.yaml'), 'r') as file:
             data = yaml.safe_load(file)
 
         output = kbuild.kql_single_table_builder(data, kql_ago, time_field='Timestamp')
