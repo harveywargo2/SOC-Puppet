012d38eb272acce67a49e16a221d034e6d83b488
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-03-21 07:30:34 -0400

    control update

--- socpuppet/detectpy/windows_os/control/control.py
+++ socpuppet/detectpy/windows_os/control/control.py
@@ -36,3 +36,17 @@ def control_p1001_executing_dll_file(*, type='m365d', kql_ago='1d'):
 
     return output
 
+
+def control_p1002_executing_file_via_rundll(*, type='m365d', kql_ago='1d'):
+
+    if type == 'm365d':
+        with open(os.path.join(control_kql_path,
+                               'control_p1002_executing_file_via_rundll.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        output = kbuild.kql_single_table_builder(data, kql_ago, time_field='Timestamp')
+    else:
+        output = f'type={type} not supported'
+
+    return output
+
