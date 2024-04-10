ff7c9e5519e7fea7afdc082bbfec6700eda46e55
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-04-10 18:18:53 -0400

    esentutl

--- socpuppet/detectpy/windows_os/esentutl/esentutl.py
+++ socpuppet/detectpy/windows_os/esentutl/esentutl.py
@@ -40,3 +40,68 @@ def esentutl_p0001_copy_sam(*, logic='mde', lookback='1d'):
 
     return query
 
+
+def esentutl_p0002_copy_ntds(*, logic='mde', lookback='1d'):
+    """
+    esentutl copy ntds file
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(esent_mde_path(),
+                               'esentutl_pid_0002_copy_ntds.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def esentutl_p0003_create_vss(*, logic='mde', lookback='1d'):
+    """
+    esentutl create vss
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(esent_mde_path(),
+                               'esentutl_pid_0003_create_vss.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def esentutl_p0004_webcache_browser_dump(*, logic='mde', lookback='1d'):
+    """
+    esentutl webcache browser dump
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(esent_mde_path(),
+                               'esentutl_pid_0004_webcache_browser_dump.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
--- socpuppet/detectpy_deprecate/windows_os/__init__.py
+++ socpuppet/detectpy_deprecate/windows_os/__init__.py
@@ -1,4 +1,4 @@
-from socpuppet.detectpy_deprecate.windows_os.esentutl.esentutul import *
+
 from socpuppet.detectpy_deprecate.windows_os.hh.hh import *
 from socpuppet.detectpy_deprecate.windows_os.infdefaultinstall.infdefaultinstall import *
 from socpuppet.detectpy_deprecate.windows_os.mavinject.mavinject import *
