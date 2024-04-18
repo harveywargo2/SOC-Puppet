2cb6459b248e22c4848c35b1e70a9ee8e3fbd9e4
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-04-18 13:16:20 -0400

    entra id update

--- socpuppet/detectpy/azure_entraid/role_management/role_mgt.py
+++ socpuppet/detectpy/azure_entraid/role_management/role_mgt.py
@@ -19,9 +19,9 @@ def rolemgt_azmon_path():
     return output
 
 
-def rolemgt_p0001_any_global_admin(*, logic='azmon', lookback='1d'):
+def rolemgt_pid_0001_any_global_admin(*, logic='azmon', lookback='1d'):
     """
-    Any Global Admin activity
+    Global Admin
 
     :param logic: Logic Selection
     :param lookback: Lookback Time
@@ -41,9 +41,9 @@ def rolemgt_p0001_any_global_admin(*, logic='azmon', lookback='1d'):
     return query
 
 
-def rolemgt_p0002_any_global_admin_not_pim(*, logic='azmon', lookback='1d'):
+def rolemgt_pid_0002_any_global_admin_not_pim(*, logic='azmon', lookback='1d'):
     """
-    Any Global Admin Activity Outside of PIM
+    Global Admin
 
     :param logic: Logic Selection
     :param lookback: Lookback Time
@@ -63,9 +63,9 @@ def rolemgt_p0002_any_global_admin_not_pim(*, logic='azmon', lookback='1d'):
     return query
 
 
-def rolemgt_p0003_any_app_admin(*, logic='azmon', lookback='1d'):
+def rolemgt_pid_0003_any_app_admin(*, logic='azmon', lookback='1d'):
     """
-    Any App Admin activity
+    Application Administrator
 
     :param logic: Logic Selection
     :param lookback: Lookback Time
@@ -85,9 +85,9 @@ def rolemgt_p0003_any_app_admin(*, logic='azmon', lookback='1d'):
     return query
 
 
-def rolemgt_p0004_any_app_admin_not_pim(*, logic='azmon', lookback='1d'):
+def rolemgt_pid_0004_any_app_admin_not_pim(*, logic='azmon', lookback='1d'):
     """
-    Any App Admin activity
+    Application Administrator
 
     :param logic: Logic Selection
     :param lookback: Lookback Time
@@ -107,9 +107,9 @@ def rolemgt_p0004_any_app_admin_not_pim(*, logic='azmon', lookback='1d'):
     return query
 
 
-def rolemgt_p0005_any_app_dev(*, logic='azmon', lookback='1d'):
+def rolemgt_pid_0005_any_app_dev(*, logic='azmon', lookback='1d'):
     """
-    Any App Dev activity
+    Application Developer
 
     :param logic: Logic Selection
     :param lookback: Lookback Time
@@ -129,9 +129,9 @@ def rolemgt_p0005_any_app_dev(*, logic='azmon', lookback='1d'):
     return query
 
 
-def rolemgt_p0006_any_app_dev_not_pim(*, logic='azmon', lookback='1d'):
+def rolemgt_pid_0006_any_app_dev_not_pim(*, logic='azmon', lookback='1d'):
     """
-    Any App Dev activity no PIM
+    Application Developer
 
     :param logic: Logic Selection
     :param lookback: Lookback Time
@@ -151,9 +151,9 @@ def rolemgt_p0006_any_app_dev_not_pim(*, logic='azmon', lookback='1d'):
     return query
 
 
-def rolemgt_p0007_any_auth_admin(*, logic='azmon', lookback='1d'):
+def rolemgt_pid_0007_any_auth_admin(*, logic='azmon', lookback='1d'):
     """
-    Any auth admin
+    Authentication Administrator
 
     :param logic: Logic Selection
     :param lookback: Lookback Time
@@ -173,9 +173,9 @@ def rolemgt_p0007_any_auth_admin(*, logic='azmon', lookback='1d'):
     return query
 
 
-def rolemgt_p0008_any_auth_admin_not_pim(*, logic='azmon', lookback='1d'):
+def rolemgt_pid_0008_any_auth_admin_not_pim(*, logic='azmon', lookback='1d'):
     """
-    Any auth admin not PIM
+    Authentication Administrator
 
     :param logic: Logic Selection
     :param lookback: Lookback Time
@@ -195,9 +195,9 @@ def rolemgt_p0008_any_auth_admin_not_pim(*, logic='azmon', lookback='1d'):
     return query
 
 
-def rolemgt_p0009_any_auth_policy_admin(*, logic='azmon', lookback='1d'):
+def rolemgt_pid_0009_any_auth_policy_admin(*, logic='azmon', lookback='1d'):
     """
-    Any auth policy admin activity
+    Authentication Policy Administrator
 
     :param logic: Logic Selection
     :param lookback: Lookback Time
@@ -217,9 +217,9 @@ def rolemgt_p0009_any_auth_policy_admin(*, logic='azmon', lookback='1d'):
     return query
 
 
-def rolemgt_p0010_any_auth_policy_admin_not_pim(*, logic='azmon', lookback='1d'):
+def rolemgt_pid_0010_any_auth_policy_admin_not_pim(*, logic='azmon', lookback='1d'):
     """
-    Any auth policy admin activity not from pim
+    Authentication Policy Administrator
 
     :param logic: Logic Selection
     :param lookback: Lookback Time
@@ -239,9 +239,9 @@ def rolemgt_p0010_any_auth_policy_admin_not_pim(*, logic='azmon', lookback='1d')
     return query
 
 
-def rolemgt_p0011_any_auth_extension_admin(*, logic='azmon', lookback='1d'):
+def rolemgt_pid_0011_any_auth_extension_admin(*, logic='azmon', lookback='1d'):
     """
-    Any auth extension admin activity
+    Authentication Extensibility Administrator
 
     :param logic: Logic Selection
     :param lookback: Lookback Time
@@ -261,9 +261,9 @@ def rolemgt_p0011_any_auth_extension_admin(*, logic='azmon', lookback='1d'):
     return query
 
 
-def rolemgt_p0012_any_auth_extension_admin_not_pim(*, logic='azmon', lookback='1d'):
+def rolemgt_pid_0012_any_auth_extension_admin_not_pim(*, logic='azmon', lookback='1d'):
     """
-    Any auth extension admin activity not pim
+    Authentication Extensibility Administrator
 
     :param logic: Logic Selection
     :param lookback: Lookback Time
@@ -281,3 +281,268 @@ def rolemgt_p0012_any_auth_extension_admin_not_pim(*, logic='azmon', lookback='1
         query = f'pointer={logic} not supported'
 
     return query
+
+
+def rolemgt_pid_0013_any_ief_keyset_admin(*, logic='azmon', lookback='1d'):
+    """
+    B2C IEF Keyset Administrator
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(rolemgt_azmon_path(),
+                               'rolemgt_pid_0013_any_fed_keyset_admin.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def rolemgt_pid_0014_any_ief_keyset_admin_not_pim(*, logic='azmon', lookback='1d'):
+    """
+    B2C IEF Keyset Administrator
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(rolemgt_azmon_path(),
+                               'rolemgt_pid_0014_any_fed_keyset_admin_not_pim.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def rolemgt_pid_0015_any_billing_admin(*, logic='azmon', lookback='1d'):
+    """
+    Billing Administrator
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(rolemgt_azmon_path(),
+                               'rolemgt_pid_0015_any_billing_admin.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def rolemgt_pid_0016_any_billing_admin_not_pim(*, logic='azmon', lookback='1d'):
+    """
+    Billing Administrator
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(rolemgt_azmon_path(),
+                               'rolemgt_pid_0016_any_billing_admin_not_pim.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def rolemgt_pid_0017_any_cloudapp_security_admin(*, logic='azmon', lookback='1d'):
+    """
+    Cloud App Security Administrator
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(rolemgt_azmon_path(),
+                               'rolemgt_pid_0017_any_cloudapp_security_admin.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def rolemgt_pid_0018_any_cloudapp_security_admin_not_pim(*, logic='azmon', lookback='1d'):
+    """
+    Cloud App Security Administrator
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(rolemgt_azmon_path(),
+                               'rolemgt_pid_0018_any_cloudapp_security_admin_not_pim.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def rolemgt_pid_0019_any_cloudapp_admin(*, logic='azmon', lookback='1d'):
+    """
+    Cloud App Administrator
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(rolemgt_azmon_path(),
+                               'rolemgt_pid_0019_any_cloudapp_admin.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def rolemgt_pid_0020_any_cloudapp_admin(*, logic='azmon', lookback='1d'):
+    """
+    Cloud App Administrator
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(rolemgt_azmon_path(),
+                               'rolemgt_pid_0020_any_cloudapp_admin_not_pim.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def rolemgt_pid_0021_any_cloud_device_admin(*, logic='azmon', lookback='1d'):
+    """
+    Cloud Device Admin
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(rolemgt_azmon_path(),
+                               'rolemgt_pid_0021_any_cloud_device_admin.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def rolemgt_pid_0022_any_cloud_device_admin_not_pim(*, logic='azmon', lookback='1d'):
+    """
+    Cloud Device Admin
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(rolemgt_azmon_path(),
+                               'rolemgt_pid_0022_any_cloud_device_admin_not_pim.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def rolemgt_pid_0023_any_cap_admin(*, logic='azmon', lookback='1d'):
+    """
+    CAP Admin
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(rolemgt_azmon_path(),
+                               'rolemgt_pid_0023_any_cap_admin.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def rolemgt_pid_0024_any_cap_admin_not_pim(*, logic='azmon', lookback='1d'):
+    """
+    CAP Admin
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(rolemgt_azmon_path(),
+                               'rolemgt_pid_0024_any_cap_admin_not_pim.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
