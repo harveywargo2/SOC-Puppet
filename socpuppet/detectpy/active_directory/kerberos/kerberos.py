ea16183c1f54381a6fbc2b01ae09953743093858
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-04-23 14:47:44 -0400

    kerb

--- socpuppet/detectpy/active_directory/kerberos/kerberos.py
+++ socpuppet/detectpy/active_directory/kerberos/kerberos.py
@@ -19,9 +19,9 @@ def kerb_azmon_path():
     return output
 
 
-def kerb_pid_0001_4768_fail_bad_acnt_single_ip(*, logic='azmon', lookback='1d'):
+def kerb_pid_0001_4768_kdc_err_c_principal_unknown(*, logic='azmon', lookback='1d'):
     """
-    4768 Kerb Auth Fail w/ bad account from single IP
+    4768 Kerb Auth Fail
 
     :param logic: Logic Selection
     :param lookback: Lookback Time
@@ -30,7 +30,7 @@ def kerb_pid_0001_4768_fail_bad_acnt_single_ip(*, logic='azmon', lookback='1d'):
 
     if logic == 'azmon':
         with open(os.path.join(kerb_azmon_path(),
-                               'kerb_pid_0001_4768_fail_bad_acnt_single_ip.yaml'), 'r') as file:
+                               'kerb_pid_0001_4768_kdc_err_c_principal_unknown.yaml'), 'r') as file:
             data = yaml.safe_load(file)
 
         query = soc.detectpy.azmon_query_builder(data, lookback)
@@ -41,9 +41,9 @@ def kerb_pid_0001_4768_fail_bad_acnt_single_ip(*, logic='azmon', lookback='1d'):
     return query
 
 
-def kerb_pid_0002_4771_fail_bad_pword_single_ip(*, logic='azmon', lookback='1d'):
+def kerb_pid_0002_4768_kdc_err_policy(*, logic='azmon', lookback='1d'):
     """
-    4771 Kerb Auth Fail w/ bad account from single IP
+    4768 Kerb Auth Fail
 
     :param logic: Logic Selection
     :param lookback: Lookback Time
@@ -52,7 +52,117 @@ def kerb_pid_0002_4771_fail_bad_pword_single_ip(*, logic='azmon', lookback='1d')
 
     if logic == 'azmon':
         with open(os.path.join(kerb_azmon_path(),
-                               'kerb_pid_0002_4771_fail_bad_pword_single_ip.yaml'), 'r') as file:
+                               'kerb_pid_0002_4768_kdc_err_policy.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def kerb_pid_0003_4768_kdc_err_client_revoked(*, logic='azmon', lookback='1d'):
+    """
+    4768 Kerb Auth Fail
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(kerb_azmon_path(),
+                               'kerb_pid_0003_4768_kdc_err_client_revoked.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def kerb_pid_0004_4768_kdc_err_key_expired(*, logic='azmon', lookback='1d'):
+    """
+    4768 Kerb Auth Fail
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(kerb_azmon_path(),
+                               'kerb_pid_0004_4768_kdc_err_key_expired.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def kerb_pid_0005_4768_kdc_local_auth_success(*, logic='azmon', lookback='1d'):
+    """
+    4768 Kerb Local Auth Success
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(kerb_azmon_path(),
+                               'kerb_pid_0005_4768_kdc_local_auth_success.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def kerb_pid_0006_4768_kdc_local_auth_failure(*, logic='azmon', lookback='1d'):
+    """
+    4768 Kerb Local Auth Failure
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(kerb_azmon_path(),
+                               'kerb_pid_0006_4768_kdc_local_auth_failure.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def kerb_pid_0007_4768_kdc_logon_without_pre_auth(*, logic='azmon', lookback='1d'):
+    """
+    4768 Kerb Ticket Without Pre Auth
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(kerb_azmon_path(),
+                               'kerb_pid_0007_4768_kdc_logon_without_pre_auth.yaml'), 'r') as file:
             data = yaml.safe_load(file)
 
         query = soc.detectpy.azmon_query_builder(data, lookback)
--- socpuppet/detectpy/active_directory/kerberos/ref_kerberos.md
+++ socpuppet/detectpy/active_directory/kerberos/ref_kerberos.md
@@ -5,3 +5,5 @@
   - https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-10/security/threat-protection/auditing/audit-kerberos-authentication-service
   - https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-10/security/threat-protection/auditing/event-4768
   - https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-10/security/threat-protection/auditing/event-4771
+- Audit Kerberos Service Ticket Operations
+  - https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-10/security/threat-protection/auditing/audit-kerberos-service-ticket-operations
