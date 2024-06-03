eea20dfe4eeff7cb696516a99355c9d1df9d8043
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-06-03 11:16:08 -0400

    gsec dump update and refactor to t1003 and s0008

---
+++ socpuppet/detectpy/attack_id/t1003/__init__.py
@@ -0,0 +1,4 @@
+from socpuppet.detectpy.attack_id.t1003.gsecdump import *
+from socpuppet.detectpy.attack_id.t1003.t1003 import *
+
+
---
+++ socpuppet/detectpy/attack_id/t1003/gsecdump.py
@@ -0,0 +1,247 @@
+import yaml
+import os
+import socpuppet as soc
+from socpuppet.detectpy.attack_id.t1003.t1003 import t1003_mde_path as mde_path
+
+
+def gsecdump_pid_0001_dump_memory(*, logic='mde', lookback='1d'):
+    """
+    gsecdump hacktool
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'gsecdump_pid_0001_dump_memory.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def gsecdump_pid_0002_dump_all_flag(*, logic='mde', lookback='1d'):
+    """
+    gsecdump hacktool
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'gsecdump_pid_0002_dump_all_flag.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def gsecdump_pid_0003_dump_lsa_flag(*, logic='mde', lookback='1d'):
+    """
+    gsecdump hacktool
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'gsecdump_pid_0003_dump_lsa_flag.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def gsecdump_pid_0004_dump_hashes_flag(*, logic='mde', lookback='1d'):
+    """
+    gsecdump hacktool
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'gsecdump_pid_0004_dump_hashes_flag.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def gsecdump_pid_0005_dump_used_hash_flag(*, logic='mde', lookback='1d'):
+    """
+    gsecdump hacktool
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'gsecdump_pid_0005_dump_used_hash_flag.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def gsecdump_pid_0006_dump_flags_read_process_memory(*, logic='mde', lookback='1d'):
+    """
+    gsecdump hacktool
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'gsecdump_pid_0006_dump_flags_read_process_memory.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def gsecdump_pid_0007_dump_flags_write_lsass_memory(*, logic='mde', lookback='1d'):
+    """
+    gsecdump hacktool
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'gsecdump_pid_0007_dump_flags_write_lsass_memory.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def gsecdump_pid_0008_file_indicator(*, logic='mde', lookback='1d'):
+    """
+    gsecdump hacktool
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'gsecdump_pid_0008_file_indicator.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def gsecdump_pid_0009_av_hacktool_eventlog(*, logic='mde', lookback='1d'):
+    """
+    gsecdump hacktool
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'gsecdump_pid_0009_av_hacktool_eventlog.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def gsecdump_pid_0010_av_hacktool_alert(*, logic='mde', lookback='1d'):
+    """
+    gsecdump hacktool
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'gsecdump_pid_0010_av_hacktool_alert.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def gsecdump_pid_0011_av_hacktool_alert(*, logic='mde', lookback='1d'):
+    """
+    gsecdump hacktool
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'gsecdump_pid_0011_av_hacktool_alert.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
