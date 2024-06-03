3bbc9a6e2aada13a3b2ba87bc438edc893fff4c6
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-06-03 18:06:15 -0400

    t1003 nppspy updates

--- socpuppet/detectpy/attack_id/t1003/__init__.py
+++ socpuppet/detectpy/attack_id/t1003/__init__.py
@@ -2,5 +2,6 @@ from socpuppet.detectpy.attack_id.t1003.gsecdump import *
 from socpuppet.detectpy.attack_id.t1003.lsass import *
 from socpuppet.detectpy.attack_id.t1003.nppspy import *
 from socpuppet.detectpy.attack_id.t1003.t1003 import *
+from socpuppet.detectpy.attack_id.t1003.t1003_builtin import *
 
 
---
+++ socpuppet/detectpy/attack_id/t1003/t1003_builtin.py
@@ -0,0 +1,26 @@
+import yaml
+import os
+import socpuppet as soc
+from socpuppet.detectpy.attack_id.t1003.t1003 import t1003_mde_path as mde_path
+
+
+def builtin_pid_0001_edr_steal_creds_alert(*, logic='mde', lookback='1d'):
+    """
+    builtin MDE EDR Detection stealing creds
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'builtin_pid_0001_edr_steal_creds_alert.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
