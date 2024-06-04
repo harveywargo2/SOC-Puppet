a935ee3f69b880a4ef2ef6b5ad5a7e81990375b7
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-06-04 12:22:34 -0400

    rpcping update

--- socpuppet/detectpy/attack_id/t1003/__init__.py
+++ socpuppet/detectpy/attack_id/t1003/__init__.py
@@ -1,6 +1,7 @@
 from socpuppet.detectpy.attack_id.t1003.appcmd import *
 from socpuppet.detectpy.attack_id.t1003.comsvcs import *
 from socpuppet.detectpy.attack_id.t1003.gsecdump import *
+from socpuppet.detectpy.attack_id.t1003.rpcping import *
 from socpuppet.detectpy.attack_id.t1003.lsass import *
 from socpuppet.detectpy.attack_id.t1003.nppspy import *
 from socpuppet.detectpy.attack_id.t1003.t1003 import *
