78b8b550befb357429154b84bbe76bd809258f52
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-06-04 11:23:40 -0400

    update

--- socpuppet/detectpy/windows_os/__init__.py
+++ socpuppet/detectpy/windows_os/__init__.py
@@ -1,4 +1,3 @@
-import socpuppet.detectpy.windows_os.appcmd
 import socpuppet.detectpy.windows_os.appinstaller
 import socpuppet.detectpy.windows_os.cmstp
 import socpuppet.detectpy.windows_os.control
@@ -6,7 +5,6 @@ import socpuppet.detectpy.windows_os.dumpert
 import socpuppet.detectpy.windows_os.esentutl
 import socpuppet.detectpy.windows_os.hh
 import socpuppet.detectpy.windows_os.infdefaultinstall
-import socpuppet.detectpy.windows_os.keymgr
 import socpuppet.detectpy.windows_os.lsass
 import socpuppet.detectpy.windows_os.mavinject
 import socpuppet.detectpy.windows_os.mimikatz
