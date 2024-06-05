2ddcf014057d1f63c9026d62ba8cc3307e6516f7
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-06-05 10:14:29 -0400

    update

--- socpuppet/detectpy/attack_id/t1003/nppspy.py
+++ socpuppet/detectpy/attack_id/t1003/nppspy.py
@@ -4,7 +4,7 @@ import socpuppet as soc
 from socpuppet.detectpy.attack_id.t1003.t1003 import t1003_mde_path as mde_path
 
 
-def nppspy_pid_0001_av_pua_presenoker_alert(*, logic='mde', lookback='1d'):
+def nppspy_pid_0001_static_registry_indicator(*, logic='mde', lookback='1d'):
     """
     nppspy
 
@@ -15,7 +15,7 @@ def nppspy_pid_0001_av_pua_presenoker_alert(*, logic='mde', lookback='1d'):
 
     if logic == 'mde':
         with open(os.path.join(mde_path(),
-                               'nppspy_pid_0001_av_pua_presenoker_alert.yaml'), 'r') as file:
+                               'nppspy_pid_0001_static_registry_indicator.yaml'), 'r') as file:
             data = yaml.safe_load(file)
 
         query = soc.detectpy.mde_query_builder(data, lookback)
@@ -26,7 +26,7 @@ def nppspy_pid_0001_av_pua_presenoker_alert(*, logic='mde', lookback='1d'):
     return query
 
 
-def nppspy_pid_0002_av_pua_presenoker_dynamic(*, logic='mde', lookback='1d'):
+def nppspy_pid_0002_reg_key_value_indicator(*, logic='mde', lookback='1d'):
     """
     nppspy
 
@@ -37,73 +37,7 @@ def nppspy_pid_0002_av_pua_presenoker_dynamic(*, logic='mde', lookback='1d'):
 
     if logic == 'mde':
         with open(os.path.join(mde_path(),
-                               'nppspy_pid_0002_av_pua_presenoker_dynamic.yaml'), 'r') as file:
-            data = yaml.safe_load(file)
-
-        query = soc.detectpy.mde_query_builder(data, lookback)
-
-    else:
-        query = f'pointer={logic} not supported'
-
-    return query
-
-
-def nppspy_pid_0003_av_pua_presenoker_event(*, logic='mde', lookback='1d'):
-    """
-    nppspy
-
-    param logic: Logic Selection
-    :param lookback: Lookback Time
-    :return: Pandas Dataframe of Results
-    """
-
-    if logic == 'mde':
-        with open(os.path.join(mde_path(),
-                               'nppspy_pid_0003_av_pua_presenoker_event.yaml'), 'r') as file:
-            data = yaml.safe_load(file)
-
-        query = soc.detectpy.mde_query_builder(data, lookback)
-
-    else:
-        query = f'pointer={logic} not supported'
-
-    return query
-
-
-def nppspy_pid_0004_static_registry_indicator(*, logic='mde', lookback='1d'):
-    """
-    nppspy
-
-    param logic: Logic Selection
-    :param lookback: Lookback Time
-    :return: Pandas Dataframe of Results
-    """
-
-    if logic == 'mde':
-        with open(os.path.join(mde_path(),
-                               'nppspy_pid_0004_static_registry_indicator.yaml'), 'r') as file:
-            data = yaml.safe_load(file)
-
-        query = soc.detectpy.mde_query_builder(data, lookback)
-
-    else:
-        query = f'pointer={logic} not supported'
-
-    return query
-
-
-def nppspy_pid_0005_reg_key_value_indicator(*, logic='mde', lookback='1d'):
-    """
-    nppspy
-
-    param logic: Logic Selection
-    :param lookback: Lookback Time
-    :return: Pandas Dataframe of Results
-    """
-
-    if logic == 'mde':
-        with open(os.path.join(mde_path(),
-                               'nppspy_pid_0005_reg_key_value_indicator.yaml'), 'r') as file:
+                               'nppspy_pid_0002_reg_key_value_indicator.yaml'), 'r') as file:
             data = yaml.safe_load(file)
 
         query = soc.detectpy.mde_query_builder(data, lookback)
