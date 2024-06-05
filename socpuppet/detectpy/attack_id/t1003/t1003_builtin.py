fb5169f43a4e772ef334635dd43eee4a41ca46fb
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-06-05 09:44:43 -0400

    t1003 update

--- socpuppet/detectpy/attack_id/t1003/t1003_builtin.py
+++ socpuppet/detectpy/attack_id/t1003/t1003_builtin.py
@@ -69,3 +69,134 @@ def builtin_pid_0003_edr_cred_memory_read_alert(*, logic='mde', lookback='1d'):
 
     return query
 
+
+def builtin_pid_0004_av_hacktool_atpminidump_alert(*, logic='mde', lookback='1d'):
+    """
+    builtin MDE AV
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'builtin_pid_0004_av_hacktool_atpminidump_alert.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def builtin_pid_0005_av_hacktool_atpminidump_dynamic(*, logic='mde', lookback='1d'):
+    """
+    builtin MDE AV
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'builtin_pid_0005_av_hacktool_atpminidump_dynamic.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def builtin_pid_0006_av_hacktool_atpminidump_event(*, logic='mde', lookback='1d'):
+    """
+    builtin MDE AV
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'builtin_pid_0006_av_hacktool_atpminidump_event.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def builtin_pid_0007_av_rundlllolbin_alert(*, logic='mde', lookback='1d'):
+    """
+    builtin MDE AV
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'builtin_pid_0007_av_rundlllolbin_alert.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def builtin_pid_0008_av_rundlllolbin_dynamic(*, logic='mde', lookback='1d'):
+    """
+    builtin MDE AV
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'builtin_pid_0008_av_rundlllolbin_dynamic.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def builtin_pid_0009_av_rundlllolbin_event(*, logic='mde', lookback='1d'):
+    """
+    builtin MDE AV
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'builtin_pid_0009_av_rundlllolbin_event.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
