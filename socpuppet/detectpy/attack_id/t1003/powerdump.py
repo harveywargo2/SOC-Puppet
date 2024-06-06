c909fad6e7f178629adac1f044b7e89f034eb676
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-06-06 19:01:19 -0400

    update

--- socpuppet/detectpy/attack_id/t1003/powerdump.py
+++ socpuppet/detectpy/attack_id/t1003/powerdump.py
@@ -24,3 +24,25 @@ def powerdump_pid_0001_pshell_invoke(*, logic='mde', lookback='1d'):
         query = f'pointer={logic} not supported'
 
     return query
+
+
+def powerdump_pid_0002_pshell_commands(*, logic='mde', lookback='1d'):
+    """
+    powerdump
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mde_path(),
+                               'powerdump_pid_0002_pshell_commands.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
