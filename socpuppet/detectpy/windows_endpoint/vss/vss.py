54bf1e05ee841ecf2b4db0ee3cbeaaf377edf9d0
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-06-24 17:25:02 -0400

    update

--- socpuppet/detectpy/windows_endpoint/vss/vss.py
+++ socpuppet/detectpy/windows_endpoint/vss/vss.py
@@ -7,9 +7,9 @@ cpath = os.path.dirname(os.path.abspath(__file__))
 mpath = os.path.join(cpath, 'mde')
 
 
-def vssadmin_pid_0001_create_shadow(*, logic='mde', lookback='1d'):
+def diskshadow_pid_0001_shadowcopy(*, logic='mde', lookback='1d'):
     """
-    vssadmin
+    volume shadow copy
 
     param logic: Logic Selection
     :param lookback: Lookback Time
@@ -18,29 +18,7 @@ def vssadmin_pid_0001_create_shadow(*, logic='mde', lookback='1d'):
 
     if logic == 'mde':
         with open(os.path.join(mpath,
-                               'vssadmin_pid_0001_create_shadow.yaml'), 'r') as file:
-            data = yaml.safe_load(file)
-
-        query = soc.detectpy.mde_query_builder(data, lookback)
-
-    else:
-        query = f'pointer={logic} not supported'
-
-    return query
-
-
-def vssadmin_pid_0002_create_shadow_c_drive(*, logic='mde', lookback='1d'):
-    """
-    vssadmin
-
-    param logic: Logic Selection
-    :param lookback: Lookback Time
-    :return: Pandas Dataframe of Results
-    """
-
-    if logic == 'mde':
-        with open(os.path.join(mpath,
-                               'vssadmin_pid_0002_create_shadow_c_drive.yaml'), 'r') as file:
+                               'diskshadow_pid_0001_shadowcopy.yaml'), 'r') as file:
             data = yaml.safe_load(file)
 
         query = soc.detectpy.mde_query_builder(data, lookback)
@@ -159,3 +137,48 @@ def vss_pid_0005_globalroot_shadowcopy(*, logic='mde', lookback='1d'):
         query = f'pointer={logic} not supported'
 
     return query
+
+
+def vssadmin_pid_0001_create_shadow(*, logic='mde', lookback='1d'):
+    """
+    vssadmin
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mpath,
+                               'vssadmin_pid_0001_create_shadow.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def vssadmin_pid_0002_create_shadow_c_drive(*, logic='mde', lookback='1d'):
+    """
+    vssadmin
+
+    param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'mde':
+        with open(os.path.join(mpath,
+                               'vssadmin_pid_0002_create_shadow_c_drive.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.mde_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
