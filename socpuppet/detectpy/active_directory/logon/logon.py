6c35b3513d05b9a5007abc9bc69b6b868c2b8370
Harvey Wargo 2 <16654472+harveywargo2@users.noreply.github.com>
2024-07-03 14:56:07 -0400

    update Active Directory

--- socpuppet/detectpy/active_directory/logon/logon.py
+++ socpuppet/detectpy/active_directory/logon/logon.py
@@ -225,3 +225,311 @@ def logon_pid_0010_2625_status_smartcard_not_authenticated(*, logic='azmon', loo
         query = f'pointer={logic} not supported'
 
     return query
+
+
+def logon_pid_0011_4625_status_smartcard_no_card(*, logic='azmon', lookback='1d'):
+    """
+    Logon
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(azpath,
+                               'logon_pid_0011_4625_status_smartcard_no_card.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def logon_pid_0012_4625_status_smartcard_no_key_container(*, logic='azmon', lookback='1d'):
+    """
+    Logon
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(azpath,
+                               'logon_pid_0012_4625_status_smartcard_no_key_container.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def logon_pid_0013_4625_status_smartcard_no_certificates(*, logic='azmon', lookback='1d'):
+    """
+    Logon
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(azpath,
+                               'logon_pid_0013_4625_status_smartcard_no_certificates.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def logon_pid_0014_4625_status_smartcard_no_keyset(*, logic='azmon', lookback='1d'):
+    """
+    Logon
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(azpath,
+                               'logon_pid_0014_4625_status_smartcard_no_keyset.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def logon_pid_0015_4625_status_smartcard_card_expired(*, logic='azmon', lookback='1d'):
+    """
+    Logon
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(azpath,
+                               'logon_pid_0015_4625_status_smartcard_card_expired.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def logon_pid_0016_4625_status_pwd_expired(*, logic='azmon', lookback='1d'):
+    """
+    Logon
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(azpath,
+                               'logon_pid_0016_4625_status_pwd_expired.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def logon_pid_0017_4625_status_smartcard_logon_required(*, logic='azmon', lookback='1d'):
+    """
+    Logon
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(azpath,
+                               'logon_pid_0017_4625_status_smartcard_logon_required.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def logon_pid_0018_4625_status_sync_required(*, logic='azmon', lookback='1d'):
+    """
+    Logon
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(azpath,
+                               'logon_pid_0018_4625_status_sync_required.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def logon_pid_0019_4625_status_dc_time_diff(*, logic='azmon', lookback='1d'):
+    """
+    Logon
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(azpath,
+                               'logon_pid_0019_4625_status_dc_time_diff.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def logon_pid_0020_4625_status_logon_type_note_granted(*, logic='azmon', lookback='1d'):
+    """
+    Logon
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(azpath,
+                               'logon_pid_0020_4625_status_logon_type_note_granted.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def logon_pid_0021_4625_status_ltrusted_relationship_fail(*, logic='azmon', lookback='1d'):
+    """
+    Logon
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(azpath,
+                               'logon_pid_0021_4625_status_ltrusted_relationship_fail.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def logon_pid_0022_4625_status_pwd_must_change(*, logic='azmon', lookback='1d'):
+    """
+    Logon
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(azpath,
+                               'logon_pid_0022_4625_status_pwd_must_change.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def logon_pid_0023_4625_status_acnt_locked_out(*, logic='azmon', lookback='1d'):
+    """
+    Logon
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(azpath,
+                               'logon_pid_0023_4625_status_acnt_locked_out.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
+
+
+def logon_pid_0024_4625_status_failure_single_ip(*, logic='azmon', lookback='1d'):
+    """
+    Logon
+
+    :param logic: Logic Selection
+    :param lookback: Lookback Time
+    :return: Pandas Dataframe of Results
+    """
+
+    if logic == 'azmon':
+        with open(os.path.join(azpath,
+                               'logon_pid_0024_4625_status_failure_single_ip.yaml'), 'r') as file:
+            data = yaml.safe_load(file)
+
+        query = soc.detectpy.azmon_query_builder(data, lookback)
+
+    else:
+        query = f'pointer={logic} not supported'
+
+    return query
